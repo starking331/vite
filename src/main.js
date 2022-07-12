4995912b6e81175462f2d94e3ee23e88d45fda40
starking331 <hathot33@gmail.com>
2022-07-12 16:50:43 +0400

    init commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import('/src/main.css')
+
+import sunglassesImg from '/assets/sunglasses.png'
+import sparklesImg from '/assets/sparkles.png'
+
+import anime from 'animejs';
+
+document.documentElement.style.setProperty('--url-sunglasses', `url("` + sunglassesImg + `")`)
+document.documentElement.style.setProperty('--url-sparkles', `url("` + sparklesImg + `")`)
+
+console.log('lox');
+
+anime({
+    targets: document.querySelector('#app > h1'),
+    rotate: '1turn',
+    duration: 3000,
+    loop: true
+});
